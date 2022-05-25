316b754f3be0c89fdbb95b0cfdeeb288998b7902
DESKTOP-VVM0KFF\HTuan <tungxm113@gmail.com>
2022-05-25 13:48:34 +0700

    first commit

---
+++ functions/api.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import serverless from 'serverless-http'
+
+const app = express()
+// RSoft Server
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello World!' })
+})
+
+module.exports.handler = serverless(app)
